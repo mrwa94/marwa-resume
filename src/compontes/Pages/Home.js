e480aff632c0d0d093f92cb83a8953b76c3a2aa9
mrwaff4 <mreaff4@gmail.com>
2024-07-28 21:14:41 +0300

    add type-animation

--- src/compontes/Pages/Home.js
+++ src/compontes/Pages/Home.js
@@ -2,6 +2,8 @@
 import { Container } from 'react-bootstrap';
 import { BiLogoGmail } from "react-icons/bi";
 import { FaLinkedinIn, FaGithubAlt, FaBehance } from "react-icons/fa6";
+import { TypeAnimation } from 'react-type-animation';
+
 
 import logo from '../../assist/logo.png'
 
@@ -25,6 +27,8 @@ export default function Home() {
       }
    ]
 
+
+
    return (
       <div name='home' className=' md:h-screen home-div'>
          <div className='mr-3'>
@@ -46,15 +50,40 @@ export default function Home() {
                   <img src={logo} alt="logo" className=" max-w-[70px]  lg:hidden " />
                   <p className='  text-teal-900 pt-4' > Hi , my name is   </p>
                </div>
-               <h1 className='lg:text-6xl md:text-4xl sm:text-xl text-[#3F5654] '>Marwa Alsubhi </h1>
-               <h2 className='lg:text-7xl md:text-5xl sm:text-xl text-[#9699a0]'> I'm a Full Stack Developer </h2>
-               <p className='text-teal-900 max-w-[700px]'> Development is my passion and expertise. As a front-end developer,
-                  I specialize in creating captivating user interfaces
-                  and seamless user experiences for websites and applications.</p>
+               <div>
+                  <h1 className='lg:text-6xl md:text-4xl sm:text-xl text-[#3F5654] '>
+                     <TypeAnimation
+                        sequence={['Marwa Alsubhi.']}
+                        speed={50}
+                        cursor={false}
+                        wrapper='div' />
+                  </h1>
+               </div>
+               {/* title */}
+               <div>
+                  <h2 className='lg:text-7xl md:text-5xl sm:text-xl text-[#9699a0]'>
+                     <TypeAnimation
+                        sequence={[1000, 'I am a Full Stack Developer']}
+                        wrapper='div'
+                        speed={50}
+                        cursor={false}
+                        repeat={Infinity} />
+                  </h2>
+               </div>
+
+               <div>
+                  <p className='text-teal-900 max-w-[700px]'> </p>
+                  <TypeAnimation
+                     sequence={[2000, 'Development is my passion and expertise. As a front-end developer. ', 'I specialize in creating captivating user interfaces and seamless user experiences for websites and applications.']}
+                     wrapper='div'
+                     repeat={Infinity}
+                     speed={50} />
+                  <p />
+               </div>
+
                <div className='button-home'>
                   {/* To download the resume    */}
-                  <a
-                     download
+                  <a download
                      href='Resume.pdf'
                      className='btn-primary group px-5 py-2'>
                      Resume
